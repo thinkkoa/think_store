8d09064b100f19fcc6e781de6f1abb75458d8280
yanglin <richenlin@gmail.com>
2017-11-16 18:48:26 +0800

    init

--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 'use strict';
 //rewite promise, bluebird is much faster
 global.Promise = require('bluebird');
-require('babel-runtime/core-js/promise').default = Promise;
 
 //export
 module.exports = require('./lib/store.js');
